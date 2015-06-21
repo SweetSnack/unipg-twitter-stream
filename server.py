40715cd5698171276b1acafe760afc54e41903ed
Federico Dolce <psykopear@gmail.com>
2015-06-21 17:57:28 +0200

    Pep8ed the previous commit

--- server.py
+++ server.py
@@ -4,28 +4,27 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-from os.path import join, dirname
-from dotenv import load_dotenv
+import os
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+consumer_key = os.environ.get("consumer_key")
+consumer_secret = os.environ.get("consumer_secret")
 
-consumer_key=os.environ.get("consumer_key")
-consumer_secret=os.environ.get("consumer_secret")
+access_token = os.environ.get("access_token")
+access_token_secret = os.environ.get("access_token_secret")
 
-access_token=os.environ.get("access_token")
-access_token_secret=os.environ.get("access_token_secret")
 
 class StdOutListener(StreamListener):
     """ A listener handles tweets are the received from the stream.
     This is a basic listener that just prints received tweets to stdout.
     """
+
     def on_data(self, data):
         print(data)
         return True
 
     def on_error(self, status):
         print(status)
+
 
 if __name__ == '__main__':
     l = StdOutListener()
