834e506561df863d37378b04e988be2195cb1cbc
Federico Dolce <psykopear@gmail.com>
2015-06-21 20:17:04 +0200

    Modified the server so it prints only the hashtag

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
-from __future__ import absolute_import, print_function
-
+from sys import stdout
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+import json
 import os
+
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 consumer_key = os.environ.get("consumer_key")
 consumer_secret = os.environ.get("consumer_secret")
@@ -12,14 +18,24 @@ consumer_secret = os.environ.get("consumer_secret")
 access_token = os.environ.get("access_token")
 access_token_secret = os.environ.get("access_token_secret")
 
+hashtags = ['#1DFoMMVA', ]
 
 class StdOutListener(StreamListener):
-    """ A listener handles tweets are the received from the stream.
+    """
+    A listener handles tweets are the received from the stream.
     This is a basic listener that just prints received tweets to stdout.
     """
 
     def on_data(self, data):
-        print(data)
+        data = json.loads(data)
+        data_hashtags = data['entities']['hashtags']
+        for i in data_hashtags:
+            hashtag = '#{}'.format(i['text'])
+            if hashtag in hashtags:
+                print(hashtag)
+                # DON'T FORGET TO FLUSH!!!!
+                stdout.flush()
+                return True
         return True
 
     def on_error(self, status):
@@ -27,9 +43,10 @@ class StdOutListener(StreamListener):
 
 
 if __name__ == '__main__':
+
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
     stream = Stream(auth, l)
-    stream.filter(track=['basketball'])
+    stream.filter(track=hashtags)
