455639a15c567872f522d3d0c6d20789fcc49935
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2015-07-11 13:28:25 +0200

    working solution using Tornado

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import json
+import threading
+
+import config
 
 from os import environ
 from os.path import join, dirname
@@ -9,20 +12,19 @@ import tornado.websocket
 import tornado.ioloop
 import tornado.web
 
+from tweepy import OAuthHandler, API, Stream, api
 from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler, API
-from tweepy import Stream, api
 
 
 # loading consumer key and access token
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-consumer_key = os.environ.get("consumer_key")
-consumer_secret = os.environ.get("consumer_secret")
+consumer_key = environ.get("consumer_key")
+consumer_secret = environ.get("consumer_secret")
 
-access_token = os.environ.get("access_token")
-access_token_secret = os.environ.get("access_token_secret")
+access_token = environ.get("access_token")
+access_token_secret = environ.get("access_token_secret")
 
 
 class WebHandler(tornado.web.RequestHandler):
@@ -33,20 +35,22 @@ class WebHandler(tornado.web.RequestHandler):
         self.render("index.html")
 
 
-class WebSocketHandler(tornado.web.WebSocketHandler):
+class MessageHandler(tornado.websocket.WebSocketHandler):
     """
     Manages websocket connections
     """
+    connections = []
+
     def open(self):
-        pass
+        self.connections.append(self)
 
     def on_close(self):
-        pass
+        self.connections.remove(self)
 
 
 handlers = [
     (r"/", WebHandler),
-    (r"/ws", WebSocketHandler),
+    (r"/ws", MessageHandler),
 ]
 
 application = tornado.web.Application(handlers)
@@ -61,7 +65,10 @@ class StdOutListener(StreamListener):
         data_hashtags = data['entities']['hashtags']
         for i in data_hashtags:
             hashtag = '#{}'.format(i['text'])
-            if hashtag in hashtags:
+            if hashtag in config.HASHTAGS:
+                # sends a websocket message with the appropriate hashtag
+                for connection in MessageHandler.connections:
+                    connection.write_message(hashtag)
                 return True
         return True
 
@@ -69,12 +76,27 @@ class StdOutListener(StreamListener):
         print(status)
 
 
-if __name__ == '__main__':
+def TwitterListener():
+    """
+    Initializes the twitter listener
+    """
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
     stream = Stream(auth, l)
     api = API(auth_handler=auth)
-    hashtags = [i['name'] for i in api.trends_place(id=44418)[0]['trends']]
-    stream.filter(track=hashtags)
+    config.HASHTAGS = [ x['name'] for x in api.trends_place(id=44418)[0]['trends'] ]
+
+    print("Stream listener is up and running")
+    stream.filter(track=config.HASHTAGS)
+
+
+if __name__ == '__main__':
+    # listening Twitter stream in a new thread
+    threading.Thread(target=TwitterListener).start()
+
+    # starting tornado
+    application.listen(8080)
+    print("Server is running on port :8080")
+    tornado.ioloop.IOLoop.current().start()
